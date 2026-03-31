a94d11334b21bb7edee897ccc8e1b8249f9ddcc0
Rahul Parmar <parmarnanubhaiv@gmail.com>
2026-03-31 16:40:52 +0530

    adding new change

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -26,7 +26,7 @@ const ProductCard = ({ product, img, loading }) => {
 
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/v1/cart/add`,
+        `${import.meta.env.VITE_URL}/api/v1/cart/add`,
         { productId },
         {
           headers: {
--- frontend/src/components/ProductDesc.jsx
+++ frontend/src/components/ProductDesc.jsx
@@ -13,7 +13,7 @@ const ProductDesc = ({ product }) => {
   const addToCart = async (productId) => {
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/v1/cart/add`,
+        `${import.meta.env.VITE_URL}/api/v1/cart/add`,
         { productId },
         {
           headers: {
