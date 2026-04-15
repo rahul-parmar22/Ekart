86d6cd8ac49c880f2b30bcc140d41e0769ec725d
Rahul Parmar <parmarnanubhaiv@gmail.com>
2026-04-16 01:00:25 +0530

    Update verifyEmail.js

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -12,6 +12,10 @@ export const verifyEmail = (email, token) => {
     },
   });
 
+               // 🔥 IMPORTANT: verify connection first (debugging)
+    await transporter.verify();
+    console.log("SMTP Connected Successfully");
+            
   // Define the email options
   const mailOptions = {
     from: process.env.MAIL_USER,
@@ -20,10 +24,12 @@ export const verifyEmail = (email, token) => {
     text: `Hi! There, You have recently visited our website and entered your email. Please follow and entered you email. Please follow the given link to verify your email https://electronicsitem.netlify.app/verify/${token} Thanks`,
   };
 
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) return console.log("Mail Error:", error); //return no lakho to pan chale ek linena code mate pan pachhi nicheno code banne successfuly vala pan console thay, jyare error aavi chhe tem chhata pan
-
+  // Send the email  //old code
+ // transporter.sendMail(mailOptions, (error, info) => {
+   // if (error) return console.log("Mail Error:", error); //return no lakho to pan chale ek linena code mate pan pachhi nicheno code banne successfuly vala pan console thay, jyare error aavi chhe tem chhata pan
+       //new code
+              const info = await transporter.sendMail(mailOptions);
+            
     console.log("Email sent:", info);
     console.log("Email sent successfully!");
   });
